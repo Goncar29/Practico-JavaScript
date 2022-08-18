2ac5569fa26c581ed955d3511e1b3b4b9838f5be
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-08-18 19:09:27 -0300

    Realizamos el analisis general y el calculo de la mediana top 10% de salarios

--- busqueda.js
+++ busqueda.js
@@ -106,3 +106,29 @@ function proyeccionPorEmpresa(nombre) {
     }
 }
 
+// Análisis general
+function medianaGeneral() {
+    const listaMedianas = salarios.map(
+        persona => medianaPorPersona(persona.name)
+    );
+    
+    const mediana = PlatziMath.calcularMediana(listaMedianas);
+
+    return mediana;
+}
+// Top 10% de salarios
+function medianaTop10() {
+    const listaMedianas = salarios.map(
+        persona => medianaPorPersona(persona.name)
+    );
+
+    const medianasOrdenadas = PlatziMath.ordenarLista(listaMedianas);
+    
+    const cantidad = listaMedianas.length / 10;
+    const limite = listaMedianas.length - cantidad;
+    
+    const top10 = medianasOrdenadas.slice(limite, medianasOrdenadas.length);
+
+    const medianaTop10 = PlatziMath.calcularMediana(top10);
+    return medianaTop10;
+}
