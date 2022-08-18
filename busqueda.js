3751c737b0d7e2902e55f9ba476ef575fa429a9c
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-08-18 14:06:59 -0300

    Restructuramos la informacion para las empresas

--- busqueda.js
+++ busqueda.js
@@ -49,3 +49,18 @@ function proyeccionPorPersona(nombrePersona) {
     return nuevoSalario;
 }
 
+for (persona of salarios) {
+    for (trabajo of persona.trabajos) {
+        if (!empresas[trabajo.empresa]) {
+            empresas[trabajo.empresa] = {};
+    }
+
+    if (!empresas[trabajo.empresa][trabajo.year]) {
+        empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+
+console.log({empresas});
