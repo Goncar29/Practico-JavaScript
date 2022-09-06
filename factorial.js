749c857a71cb2e4669596f7a23828151364b9e7d
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-09-06 17:43:19 -0300

    Se crea archivo y se calcula el factor de un numero

---
+++ factorial.js
@@ -0,0 +1,11 @@
+var numero = 1
+function factorial(n) {
+    for(let i = 1; i <= n; i++){
+        numero = numero * i;
+        console.log(n)
+        console.log(numero)
+    }
+    
+}
+
+factorial(5)
