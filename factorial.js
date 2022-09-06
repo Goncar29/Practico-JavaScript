584d556d417cfc92845af19f590ad24feb85e483
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-09-06 20:52:46 -0300

    Se arregla errores de codigo

--- factorial.js
+++ factorial.js
@@ -1,11 +1,15 @@
-var numero = 1
-function factorial(n) {
+const btn = document.getElementById('btn');
+const resultado = document.getElementById('resultado');
+
+btn.addEventListener('click', factorial());
+
+function factorial() {
+    const n = parseInt(document.getElementById('factor').value);
+    let numero = 1
     for(let i = 1; i <= n; i++){
         numero = numero * i;
         console.log(n)
         console.log(numero)
+        resultado.innerText = `El factorial de ${n} es: ${numero}`
     }
-    
 }
-
-factorial(5)
